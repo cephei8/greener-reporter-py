a313f37e8cb05303362645d76f8d26a00584baab
cephei8 <i@cephei8.dev>
2025-12-04 18:27:47 +0100

    Initial commit
    Signed-off-by: cephei8 <i@cephei8.dev>

---
+++ greener_reporter/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name="greener-reporter",
+    version="0.11.0",
+    description="Python binding for Greener Reporter",
+    url="https://greener.cephei8.dev",
+    author="cephei8",
+    author_email="",
+    python_requires=">=3.7, <4",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    packages=["greener_reporter"],
+    include_package_data=True,
+    install_requires=[
+        "pydantic",
+    ],
+)
